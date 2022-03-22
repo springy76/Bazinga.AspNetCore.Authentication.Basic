5f630e416f2eaa206cc73b5a5c922e36f45cf172
MaS <mas@dotnetfabrik.de>
2022-03-22 10:23:23 +0100

    fix: tests don't compile anymore

--- test/NegativeBasicCredentialVerifier.cs
+++ test/NegativeBasicCredentialVerifier.cs
@@ -1,9 +1,13 @@
-﻿using System.Threading.Tasks;
-
-namespace Bazinga.AspNetCore.Authentication.Basic.Tests
+﻿namespace Bazinga.AspNetCore.Authentication.Basic.Tests
 {
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+
     internal class NegativeBasicCredentialVerifier : IBasicCredentialVerifier
     {
-        public Task<bool> Authenticate(string username, string password) => Task.FromResult(false); 
+        public Task<bool> AuthenticateAsync(HttpContext context, string username, string password, ICollection<Claim> claims) => Task.FromResult(false);
     }
 }
